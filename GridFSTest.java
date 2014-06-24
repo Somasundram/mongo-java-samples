ba8dcd4d013e67d3f159a220a13afedb7b925ed1
Somasundram <Somasundram@users.noreply.github.com>
2014-06-23 22:20:12 -0500

    Update GridFSTest.java

--- GridFSTest.java
+++ GridFSTest.java
@@ -1,7 +1,9 @@
-package com.somlab.gridfs;
+ppackage com.somlab.gridfs;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 
@@ -9,31 +11,32 @@ import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.MongoClient;
 import com.mongodb.gridfs.GridFS;
+import com.mongodb.gridfs.GridFSDBFile;
 import com.mongodb.gridfs.GridFSInputFile;
 
 public class GridFSTest {
 
 	/**
 	 * @param args
-	 * @throws UnknownHostException 
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) throws UnknownHostException {
+	public static void main(String[] args) throws IOException {
 		MongoClient client = new MongoClient();
 		DB db = client.getDB("course");
 		FileInputStream inputStream = null;
 		
-		GridFS videos = new GridFS(db, "videos"); // returns GridFS bucket names "videos"
+		GridFS videos = new GridFS(db, "videos2"); // returns GridFS bucket names "videos"
 		
 		try {
 			
-			inputStream = new FileInputStream("C:\\Users\\sbalak16\\Desktop\\DATA\\Career\\Technical\\MongoDB\\MongoDB-University-Course\\CODE\\M101J\\src\\main\\resources\\client_story_video.mp4");
+			inputStream = new FileInputStream("client_story_video2.mp4");
 		} catch( FileNotFoundException e ) {
 			System.out.println("Can't open the file!");
 			e.printStackTrace();
 			System.exit(1);
 		}
 		
-		GridFSInputFile video = videos.createFile(inputStream, "client_story.mp4");
+		GridFSInputFile video = videos.createFile(inputStream, "client_story2.mp4");
 		
 		// create some meta-data for the video
 		BasicDBObject meta = new BasicDBObject("description", "Ameriprise Client Story");
@@ -48,6 +51,13 @@ public class GridFSTest {
 		video.save();
 		
 		System.out.println("Object ID in Files Collection: "+ video.get("_id") );
+		
+		/* Reading it back */
+		
+		GridFSDBFile gridFile = videos.findOne(new BasicDBObject("filename", "client_story2.mp4"));
+		
+		FileOutputStream outputStream = new FileOutputStream("client_story2_copy_from_DB.mp4");
+		gridFile.writeTo(outputStream);		
 
 	}
 
